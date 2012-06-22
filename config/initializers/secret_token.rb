a265c7575f4c0572207f4c5381bb407ce548777a
David Philipson <david.philipson@gmail.com>
2012-06-21 22:09:13 -0700

    Create repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JavaAndRobotics::Application.config.secret_token = 'e5947e07e05d3dd94e3862ff557405217b5aa42384fc256ea9fbf61426c4b5adba53d4eb64c00bc51117b0a3829d8aaa2f0231fc913ed8e62cdb8ab3215952e6'
