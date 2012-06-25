352533e2af082ef0cb63f04c9060a0ff27245af5
David Philipson <david.philipson@gmail.com>
2012-06-25 12:05:10 -0700

    Add additional home page images

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    image_files = ['wall-e.jpg',
+                   #'hal.jpg',
+                   'glados.png',
+                   #'terminator.jpg'
+                  ]
+    @image_file = image_files.sample
   end
 
   def assignments
