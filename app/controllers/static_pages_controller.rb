4145e25c2a1b7cb20cf31916457f5e37a96c6e52
David Philipson <david.philipson@gmail.com>
2012-06-25 12:17:38 -0700

    Make GLaDOS appear rarely.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
-    image_files = ['wall-e.jpg',
-                   #'hal.jpg',
-                   'glados.png',
-                   #'terminator.jpg'
-                  ]
-    @image_file = image_files.sample
+    #image_files = ['wall-e.jpg',
+    #               'hal.jpg',
+    #               'glados.png',
+    #               'terminator.jpg'
+    #              ]
+    @image_file = Random.rand(20) == 0 ? 'glados.png' : 'wall-e.jpg'
   end
 
   def assignments
